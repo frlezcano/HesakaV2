5f996c3276181177b83ddd8a13975cf2ccb32a5b
Roberto Lezcano <frlezcano@gmail.com>
2017-04-09 11:04:49 -0400

    Se agrega FOSUserBundle y sobreescritura del default template

---
+++ src/AppBundle/Entity/Usuario.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="usuario")
+ */
+class Usuario extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    protected $fechaNacimiento;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFechaNacimiento()
+    {
+        return $this->fechaNacimiento;
+    }
+
+    /**
+     * @param mixed $fechaNacimiento
+     */
+    public function setFechaNacimiento($fechaNacimiento)
+    {
+        $this->fechaNacimiento = $fechaNacimiento;
+    }
+
+}
